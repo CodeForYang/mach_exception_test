95ca07c065b78703ee3d97345a5a5ef11fc688c3
yangpei <yangpei@yy.com>
2020-09-27 16:29:39 +0800

    Initial Commit

---
+++ Mach_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mach_test
+//
+//  Created by 易家杨 on 2020/9/27.
+//  Copyright © 2020 易家杨. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
